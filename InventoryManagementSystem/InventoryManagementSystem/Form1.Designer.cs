8cc154ffbcec02b72241129d241722e172ea6ad6
David Kim <nastycoffee@hotmail.com>
2023-02-18 10:13:01 -0800

    Worked on Start Page

---
+++ InventoryManagementSystem/InventoryManagementSystem/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace InventoryManagementSystem
+{
+    partial class Window
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Window));
+            this.LoginPanel = new System.Windows.Forms.Panel();
+            this.PWLabel = new System.Windows.Forms.Label();
+            this.UserLabel = new System.Windows.Forms.Label();
+            this.LoginTitle = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.LoginPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LoginPanel
+            // 
+            this.LoginPanel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.LoginPanel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.LoginPanel.Controls.Add(this.textBox1);
+            this.LoginPanel.Controls.Add(this.PWLabel);
+            this.LoginPanel.Controls.Add(this.UserLabel);
+            this.LoginPanel.Controls.Add(this.LoginTitle);
+            this.LoginPanel.Location = new System.Drawing.Point(330, 170);
+            this.LoginPanel.Name = "LoginPanel";
+            this.LoginPanel.Size = new System.Drawing.Size(520, 566);
+            this.LoginPanel.TabIndex = 0;
+            this.LoginPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.Login_Paint);
+            // 
+            // PWLabel
+            // 
+            this.PWLabel.AutoSize = true;
+            this.PWLabel.Location = new System.Drawing.Point(215, 298);
+            this.PWLabel.Name = "PWLabel";
+            this.PWLabel.Size = new System.Drawing.Size(87, 25);
+            this.PWLabel.TabIndex = 3;
+            this.PWLabel.Text = "Password";
+            this.PWLabel.Click += new System.EventHandler(this.label1_Click_2);
+            // 
+            // UserLabel
+            // 
+            this.UserLabel.AutoSize = true;
+            this.UserLabel.Location = new System.Drawing.Point(211, 180);
+            this.UserLabel.Name = "UserLabel";
+            this.UserLabel.Size = new System.Drawing.Size(91, 25);
+            this.UserLabel.TabIndex = 2;
+            this.UserLabel.Text = "Username";
+            this.UserLabel.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // LoginTitle
+            // 
+            this.LoginTitle.AutoSize = true;
+            this.LoginTitle.Font = new System.Drawing.Font("Candara", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.LoginTitle.Location = new System.Drawing.Point(57, 40);
+            this.LoginTitle.Name = "LoginTitle";
+            this.LoginTitle.Size = new System.Drawing.Size(390, 35);
+            this.LoginTitle.TabIndex = 0;
+            this.LoginTitle.Text = "Inventory Management System";
+            this.LoginTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.LoginTitle.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(291, 237);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 31);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1180, 907);
+            this.Controls.Add(this.LoginPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Window";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.LoginPanel.ResumeLayout(false);
+            this.LoginPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel LoginPanel;
+        private Label LoginTitle;
+        private Label UserLabel;
+        private Label PWLabel;
+        private TextBox textBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+    }
+}
---
+++ InventoryManagementSystem/InventoryManagementSystem/Form1.cs
@@ -0,0 +1,30 @@
+namespace InventoryManagementSystem
+{
+    public partial class Window : Form
+    {
+        public Window()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
